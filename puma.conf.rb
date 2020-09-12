c9bd27b52d75184a05b8320dc42fc1a4694176f6
hiwayama <iwayma1880@gmail.com>
2020-09-12 04:14:17 +0000

    wip

--- puma.conf.rb
+++ puma.conf.rb
@@ -1,16 +1,21 @@
 worker_num = ENV.fetch("WORKER_NUM", 3)
 workers worker_num
+app_root = File.expand_path('../', __FILE__)
 
 thread_num = ENV.fetch("PUMA_THREAD_NUM", 5)
 threads thread_num, thread_num
 
 port (ENV.fetch('SERVER_PORT', 1323)).to_i
+pidfile "#{app_root}/server.pid"
 
 worker_timeout 60
 
 daemonize true
 
 preload_app!
+
+stdout_redirect "#{app_root}/puma.log", "#{app_root}/puma_error.log", true
+
 
 # https://github.com/puma/puma/issues/1957
 restart_command "bundle exec --keep-file-descriptors pumactl"
